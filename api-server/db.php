3cb438aa0307d4ce188e86945eccfffe4ecaafd3
Pepe Belmonte <contacto@pepebelmonte.es>
2025-11-10 19:28:52 +0100

    conexión php

--- api-server/db.php
+++ api-server/db.php
@@ -12,7 +12,7 @@
   */
 
   try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
     die("Error al conectar con la base de datos: " . $e->getMessage());
--- api-server/user.php
+++ api-server/user.php
@@ -11,15 +11,25 @@
           if (isset($_GET['username']) && $_GET['username']!="") {
             if (isset($_GET['password']) && $_GET['password']!="") {
               $username = $_GET['username'];
-              $password = base64_encode($_GET['password']);
+              //$password = base64_encode($_GET['password']);
+              $password = password_hash($data['password'], PASSWORD_DEFAULT); // codificación segura
+              // mejor recuperar el usuario y validar la contraseña que hacer consulta a BD con la contraseña
+              /*
               $stmt = $pdo->query(
                 "SELECT * FROM users WHERE username='$username' AND password='$password'"
               );
-              
-    
               $result = $stmt->fetch();
               if ($result>0) {
+              }
+              */
+              $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+              $stmt->execute([$username]);
+              $user = $stmt->fetch();
+
+              if ($user && password_verify($_GET['password'], $user['password'])) {
+                // login exitoso              
                 if ($result['active']) {
+                  // usuario activo              
                   $response['username'] = utf8_encode($result['username']);
                   $response['email'] = utf8_encode($result['email']);
                   $response['name'] = utf8_encode($result['name']);
@@ -29,11 +39,11 @@
                   echo json_encode($response);
                 }else {
                   http_response_code(401);
-                  echo json_encode(['error' => 'Usuario no activado']);  
+                  echo json_encode(['error' => 'Usuario no valido']);  
                 }
               }else {
                 http_response_code(401);
-                echo json_encode(['error' => 'Usuario y/o contraseña erroneos']);      
+                echo json_encode(['error' => 'Usuario no valido']);      
               }
             }else {
               http_response_code(401);
@@ -53,13 +63,12 @@
     case 'create':
       if ($method === 'POST'){
         // añade un nuevo usuario
-        $data = json_decode(file_get_contents('php://input'), true);
-        
+        $data = json_decode(file_get_contents('php://input'), true);        
         $username = isset($data['username'])?utf8_decode($data['username']):'';
         $email = isset($data['email'])?utf8_decode($data['email']):'';
         $name = isset($data['name'])?utf8_decode($data['name']):'';
         $surname = isset($data['surname'])?utf8_decode($data['surname']):'';
-        $password = isset($data['password'])?base64_encode($data['password']):'';
+        $password = isset($data['password'])?password_hash($data['password'], PASSWORD_DEFAULT) : '';
         $active = true;
         $token = '';
   
